ff86daf495409f80044592a4f6a69f7afed6974a
devzafarjon <devzafarjon>
2025-08-03 14:37:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,217 @@
+// -------------------------------------------
+// 1-topshiriq
+// function showAgeD(age) {
+//     console.log("Sizning yoshingiz: ", age)
+// }
+
+// showAgeD(20)
+
+// const showAgeE = function(age) {
+//     console.log("Sizning yoshingiz: ", age)
+// }
+
+// showAgeE(20)
+
+// const showAgeA = (age) => {
+//     console.log("Sizning yoshingiz: ", age)
+// }
+
+// showAgeA(20)
+
+// -------------------------------------------
+// 2-topshiriq
+// function addNumbersD(a, b) {
+//     console.log(a + b)
+// }
+
+// addNumbersD(5, 4)
+
+// const addNumbersE = function(a, b) {
+//     console.log(a + b)
+// }
+
+// addNumbersE(5, 4)
+
+// const addNumbersA =  (a, b) => {
+//     console.log(a + b)
+// }
+
+// addNumbersA(5, 4)
+
+// -------------------------------------------
+// 3-topshiriq
+// function greetD(name) {
+//     console.log("Salom, ", name)
+// }
+
+// greetD("Zafar")
+
+// const greetE = function(name) {
+//     console.log("Salom, ", name)
+// }
+
+// greetE("Zafar")
+
+// const greetA = (name) => {
+//     console.log("Salom, ", name)
+// }
+
+// greetA("Zafar")
+
+// -------------------------------------------
+// 4-topshiriq
+// function fullNameD(firstName, lastName) {
+//     console.log(firstName + " " + lastName);
+// }
+
+// fullNameD("Zafar", "Raxmonaliyev")
+
+// const fullNameE = function(firstName, lastName) {
+//     console.log(firstName + " " + lastName);
+// }
+
+// fullNameE("Zafar", "Raxmonaliyev")
+
+// const fullNameA = (firstName, lastName) => {
+//     console.log(firstName + " " + lastName);
+// }
+
+// fullNameA("Zafar", "Raxmonaliyev")
+
+// -------------------------------------------
+// 5-topshiriq
+// function rectanglePerimeterD(a, b) {
+//     console.log((a + b)*2)
+// }
+
+// rectanglePerimeterD(5, 4)
+
+// const rectanglePerimeterE = function(a, b) {
+//     console.log((a + b)*2)
+// }
+
+// rectanglePerimeterE(5, 4)
+
+// const rectanglePerimeterA = (a, b) => {
+//     console.log((a + b)*2)
+// }
+
+// rectanglePerimeterA(5, 4)
+
+// -------------------------------------------
+// 6-topshiriq
+// function triangleSidesD(a, b, c) {
+//     console.log("Tomon a: ", a);
+//     console.log("Tomon b: ", b);
+//     console.log("Tomon c: ", c);
+// }
+
+// triangleSidesD(5, 8, 7)
+
+// const triangleSidesE = function(a, b, c) {
+//     console.log("Tomon a: ", a);
+//     console.log("Tomon b: ", b);
+//     console.log("Tomon c: ", c);
+// }
+
+// triangleSidesE(5, 8, 7)
+
+// const triangleSidesA = (a, b, c) => {
+//     console.log("Tomon a: ", a);
+//     console.log("Tomon b: ", b);
+//     console.log("Tomon c: ", c);
+// }
+
+// triangleSidesA(5, 8, 7)
+
+// -------------------------------------------
+// 7-topshiriq
+// function squareNumberD(number) {
+//   console.log(Math.pow(number, 2))
+// }
+
+// squareNumberD(5)
+
+// const squareNumberE = function(number) {
+//   console.log(Math.pow(number, 2))
+// }
+
+// squareNumberE(5)
+
+// const squareNumberA = (number) => {
+//   console.log(Math.pow(number, 2))
+// }
+
+// squareNumberA(5)
+
+// -------------------------------------------
+// 8-topshiriq
+// function animalInfoD(name, age) {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+// }
+
+// animalInfoD('Mushuk', 2)
+
+// const animalInfoE = function(name, age) {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+// }
+
+// animalInfoE('Mushuk', 2)
+
+// const animalInfoA = (name, age) => {
+//     console.log(`Bu ${name} ${age} yoshda.`);
+// }
+
+// animalInfoA('Mushuk', 2)
+
+// -------------------------------------------
+// 9-topshiriq
+// function sayHalloD() {
+//   console.log("Salom Dunyo!")
+// }
+
+// sayHalloD()
+
+// const sayHalloE = function () {
+//   console.log("Salom Dunyo!")
+// }
+
+// sayHalloE()
+
+// const sayHalloA = () => {
+//   console.log("Salom Dunyo!")
+// }
+
+// sayHalloA()
+
+// -------------------------------------------
+// 10-topshiriq
+// function printNamesD(name1, name2, name3, name4, name5) {
+//   console.log(name1)
+//   console.log(name2)
+//   console.log(name3)
+//   console.log(name4)
+//   console.log(name5)
+// }
+
+// printNamesD("Ali", "Vali", "Gulbahor", "Zilola", "Javohir")
+
+// const printNamesE = function (name1, name2, name3, name4, name5) {
+//   console.log(name1)
+//   console.log(name2)
+//   console.log(name3)
+//   console.log(name4)
+//   console.log(name5)
+// }
+
+// printNamesE("Ali", "Vali", "Gulbahor", "Zilola", "Javohir")
+
+// const printNamesA = (name1, name2, name3, name4, name5) => {
+//   console.log(name1)
+//   console.log(name2)
+//   console.log(name3)
+//   console.log(name4)
+//   console.log(name5)
+// }
+
+// printNamesA("Ali", "Vali", "Gulbahor", "Zilola", "Javohir")
